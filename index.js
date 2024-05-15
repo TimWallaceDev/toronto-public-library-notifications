0e1917d5bfcd8167797a468cb1a45b2c7eb1fd08
Tim Wallace <timwallacedev@gmail.com>
2024-05-15 16:59:10 -0400

    script complete

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 import puppeteer from 'puppeteer';
 
+//library credentials
 const LIBRARY_CARD_NUMBER = process.env.LIBRARY_CARD_NUMBER;
 const LIBRARY_CARD_PIN = process.env.LIBRARY_CARD_PIN;
 
-console.log("script initiated")
+//zoho credentials
+const ZOHO_EMAIL = process.env.ZOHO_EMAIL
+const ZOHO_PASSWORD = process.env.ZOHO_PASSWORD
+
+
+const CHECK_FOR_OVERDUE = process.env.CHECK_FOR_OVERDUE
+const CHECK_FOR_DUE_SOON = process.env.CHECK_FOR_DUE_SOON
+const CHECK_FOR_READY_FOR_PICKUP = process.env.CHECK_FOR_READY_FOR_PICKUP
+const CHECK_FOR_IN_TRANSIT = process.env.CHECK_FOR_IN_TRANSIT
+
 //launch the browser
-const browser = await puppeteer.launch({ headless: false });
-console.log("browser has been launched")
+const browser = await puppeteer.launch({ headless: false, args: ['--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'] });
+
 
 //open a new page
 const page = await browser.newPage();
-console.log("new page opened")
 
 //go to the library website
 page.goto("https://account.torontopubliclibrary.ca/signin")
@@ -26,7 +35,6 @@ await page.type(userID, LIBRARY_CARD_NUMBER);
 const passwordInput = '#password';
 await page.waitForSelector(passwordInput);
 await page.type(passwordInput, LIBRARY_CARD_PIN);
-console.log("password entered");
 
 // Submit the form
 await page.keyboard.press('Enter');
@@ -34,35 +42,120 @@ await page.keyboard.press('Enter');
 //wait for page to load
 await waitForMultipleElements(page, ".sub-menu-content", 7)
 
-
 // Get the account information
 const titles = await page.$$('.sub-menu-title');
 const values = await page.$$('.sub-menu-content');
 
-console.log(titles.length, values.length)
+const data = []
 
 for (let i = 0; i < titles.length; i++) {
     // Get the text content of title and value
-    const titleText = await page.evaluate(title => title.textContent, titles[i]);
     const valueText = await page.evaluate(value => value.textContent, values[i]);
-    console.log("Title: ", titleText.trim(), " : ", valueText.trim());
+    data.push(valueText)
 }
 
 //create an object with data
 const accountInfo = {
-    overdue: false,
-    dueSoon: false,
-    dueLater: false,
-    readyForPickup: false,
-    inTransit: false,
-    activeHolds: false,
-    inactiveHolds: false
+    overdue: parseInt(data[0]),
+    dueSoon: parseInt(data[1]),
+    dueLater: parseInt(data[2]),
+    readyForPickup: parseInt(data[3]),
+    inTransit: parseInt(data[4]),
+    activeHolds: parseInt(data[5]),
+    inactiveHolds: parseInt(data[6])
 }
+
+let notify = false
+let message = ""
+// check for overdue
+if (CHECK_FOR_OVERDUE === "true") {
+    if (accountInfo.overdue > 0) {
+        notify = true
+        message += `You have ${accountInfo.overdue} books overdue. `
+    }
+}
+
+//check for due soon
+if (CHECK_FOR_DUE_SOON === "true") {
+    if (accountInfo.dueSoon > 0) {
+        notify = true
+        message += `You have ${accountInfo.dueSoon} books due soon. `
+    }
+}
+
+//check for ready for pickup
+if (CHECK_FOR_READY_FOR_PICKUP === "true") {
+    if (accountInfo.readyForPickup > 0) {
+        notify = true
+        message += `You have ${accountInfo.readyForPickup} holds ready for pickup. `
+    }
+}
+
+//check for in transit
+if (CHECK_FOR_IN_TRANSIT === "true") {
+    if (accountInfo.inTransit > 0) {
+        notify = true
+        message += `You have ${accountInfo.inTransit} holds in transit. `
+    }
+}
+
+if (notify) {
+
+}
+
+if (!message){
+    message = "look like theres nothing to notify about. You are in good standing at the Toronto Library!"
+}
+
+page.goto("https://accounts.zoho.in/signin?servicename=ZohoHome&signupurl=https://www.zoho.com/signup.html")
+await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+//enter email
+const zoho_email_input = '#login_id';
+await page.waitForSelector(zoho_email_input);
+await page.type(zoho_email_input, ZOHO_EMAIL);
+
+// Submit the email
+await page.keyboard.press('Enter');
+await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+//enter password
+const zoho_password_input = '#password';
+await page.waitForSelector(zoho_password_input);
+await page.type(zoho_password_input, ZOHO_PASSWORD);
+
+// Submit the password
+await page.keyboard.press('Enter');
+await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+// //go to mail
+// page.goto("https://mail.zohocloud.ca/zm/#mail/folder/inbox")
+// await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+//compose message
+page.goto("https://mail.zohocloud.ca/zm/#compose")
+await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+//get all input components
+await page.$$("input[type='text']")
+
+await page.keyboard.type("timwallacedev@gmail.com");
+await page.keyboard.press('Tab');
+await page.keyboard.press('Tab');
+await page.keyboard.type("Toronto Library Notification");
+await page.keyboard.press('Tab');
+
+await page.keyboard.type(message);
+
+// Press the 'Control + Shift' to send email
+await page.keyboard.down('Control');
+await page.keyboard.down('Enter');
+await page.keyboard.up('Enter');
+await page.keyboard.up('Control');
 
 
 //close the browser
-// await browser.close()
-console.log("all done")
+await browser.close()
 
 
 async function waitForMultipleElements(page, className, count) {
